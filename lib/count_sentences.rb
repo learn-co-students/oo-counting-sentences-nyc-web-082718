4f63df75fc34fa8d65111402ec413d23e37c65ac
Chris Bojemski <chris.bojemski@gmail.com>
2018-08-28 17:18:39 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.split('').last == '.'
   end
 
   def question?
-
+    self.split('').last == '?'
   end
 
   def exclamation?
-
+    self.split('').last == '!'
   end
 
   def count_sentences
-
+    count = 0
+    simpler = self.gsub(/\.+/, '.')
+    simpler = simpler.gsub(/!+/, '!')
+    simpler = simpler.gsub(/\?+/, '?')
+    puts simpler
+    simpler.split('').each do |c|
+      if ['.', '!', '?'].include? c
+        count += 1
+      end 
+    end 
+    count
   end
 end
