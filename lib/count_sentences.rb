80de22ccd740e47bce901157af7e517af82d35be
MarSchlaefer <mschlaefer00@gmail.com>
2018-08-28 19:44:54 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,34 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?('.')
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self.end_with?('?')
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?('!')
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
+    sentence_array = self.split(/\.|\?|\!/)
+    #binding.pry
+    sentence_array.delete_if {|string| string.length < 2}
 
+    sentence_array.length
   end
 end
