e026067a98042ded466a7583c24a1686d3995121
skarolk <skarolkiewicz@gmail.com>
2018-08-28 20:56:03 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,44 @@ require 'pry'
 
 class String
 
+  # def sentence?
+  #   if self[-1] == "."
+  #     true
+  #   else false
+  #   end
+  # end
+
   def sentence?
-    
+    self.end_with?(".")
   end
+
+  # def question?
+  #   if self[-1] == "?"
+  #     true
+  #   else false
+  #   end
+  # end
 
   def question?
-
+    self.end_with?("?")
   end
 
-  def exclamation?
+  # def exclamation?
+  #   if self[-1] == "!"
+  #     true
+  #   else false
+  #   end
+  # end
 
+  def exclamation?
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    delimiters = ['.', '!', '?']
+    new_array = self.split(Regexp.union(delimiters))
+    new_array.delete_if { |sentence| sentence == "" }
+    new_array.length
   end
+
 end
